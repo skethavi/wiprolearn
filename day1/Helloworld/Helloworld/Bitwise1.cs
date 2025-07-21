837ed1c9cd0d76705790086515fe93697e7e2d10
skethavi <kethavireddy@gmail.com>
2025-07-21 16:07:42 +0530

    first commit

---
+++ day1/Helloworld/Helloworld/Bitwise1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Bitwise1
+    {
+        static void Main()
+        {
+            int a = 5, b = 3;
+            //and
+            Console.WriteLine(a & b);
+            //or
+            Console.WriteLine(a | b);
+            //xor
+            Console.WriteLine(a ^ b);
+            //not
+            Console.WriteLine(~a);
+            Console.WriteLine(~7);
+            Console.WriteLine(~-3);
+        }
+    }
+}
